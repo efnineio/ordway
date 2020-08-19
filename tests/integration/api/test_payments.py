1471d15c00a9f4ecc3532ff0620954ada025ec98
nicholas-ramsey <me@nicholas-ramsey.com>
2020-08-19 18:25:21 -0500

    Removed integration tests until we can properly filter out sensitive info

--- tests/integration/api/test_payments.py
+++
@@ -1,8 +0,0 @@
-from ..base import VCRIntegrationTestCase
-
-
-class TestPayments(VCRIntegrationTestCase):
-    def test_list(self):
-        payments = self.client.payments.list(page=1, size=5)
-
-        next(payments)
--- tests/integration/api/test_products.py
+++
@@ -1,6 +0,0 @@
-from ..base import VCRIntegrationTestCase
-
-
-class TestProducts(VCRIntegrationTestCase):
-    def test_list(self):
-        self.client.products.list(page=1, size=5)
