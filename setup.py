f376033334cea706d45a2dfc9e027cafbffbc422
Nicholas Ramsey <me@nicholas-ramsey.com>
2020-08-28 12:27:46 -0500

    Updated project_urls

--- setup.py
+++ setup.py
@@ -26,12 +26,12 @@ setup(
     author="Geoffrey Doempke",
     author_email="nicholas@logram.io",
     python_requires=">=3.6",
-    url="https://github.com/efnineio/ordway",  # TODO
+    url="https://github.com/efnineio/ordway",
     packages=find_packages(exclude=["tests", "tests.*", "docs"]),
     install_requires=requirements,
     extras_require={"testing": ["tox==3.17.1"]},
     project_urls={
-        "Documentation": "",  # TODO
+        "Documentation": "https://github.com/efnineio/ordway/blob/master/README.md", 
         "Source": "https://github.com/efnineio/ordway",
         "Tracker": "https://github.com/efnineio/ordway/issues",
         "Changelog": "https://github.com/efnineio/ordway/blob/master/CHANGES.md",
@@ -41,7 +41,7 @@ setup(
         "Development Status :: 2 - Pre-Alpha",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",  # TODO
+        "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
